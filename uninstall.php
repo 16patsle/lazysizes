30b5e179e05d50f261cb3552e11eecc4d7f7fd70
Patrick Sletvold <patricksletvold@hotmail.com>
2019-02-16 14:00:04 +0100

    Fix phpcs errors

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,9 @@
 <?php
-// If uninstall is not called from WordPress, exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
+// If uninstall is not called from WordPress, exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
 }
 
-foreach (array('lazysizes_general','lazysizes_effects','lazysizes_addons','lazysizes_version') as $option) {
-	delete_option($option);
+foreach ( array( 'lazysizes_general', 'lazysizes_effects', 'lazysizes_addons', 'lazysizes_version' ) as $lazysizes_option ) {
+	delete_option( $lazysizes_option );
 }
