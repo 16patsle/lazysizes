4ae84ff5324c0d31b0289352a8be0b3afb47ee27
Patrick Sletvold <patricksletvold@hotmail.com>
2020-01-31 11:46:52 +0100

    Fix phpcs errors

--- inc/Lazysizes/class-plugincore.php
+++ inc/Lazysizes/class-plugincore.php
@@ -52,7 +52,7 @@ class PluginCore {
 
 		// If we're in the admin area, and not processing an ajax call, load the settings class.
 		if ( is_admin() && ! wp_doing_ajax() ) {
-			require dirname( __FILE__ ) . '/class-lazysizessettings.php';
+			require dirname( __FILE__ ) . '/class-settings.php';
 			$settings_class = new Settings();
 			// If this is the first time we've enabled the plugin, setup default settings.
 			register_activation_hook( __FILE__, array( $settings_class, 'first_time_activation' ) );
@@ -63,7 +63,7 @@ class PluginCore {
 			$this->settings = $this->get_settings();
 			$this->dir      = plugin_dir_url( $pluginfile );
 
-			require dirname( __FILE__ ) . '/class-lazysizespregreplace.php';
+			require dirname( __FILE__ ) . '/class-pregreplace.php';
 			$this->replace_class = new PregReplace( $this->settings, $pluginfile );
 
 			// Add inline css to head, part of noscript support.
