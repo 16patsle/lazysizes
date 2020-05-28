a3cb6a7ade36eba8db688665b503f686ba486faa
Patrick Sletvold <patricksletvold@hotmail.com>
2020-05-28 15:24:17 +0200

    Set up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import legacy from '@rollup/plugin-legacy';
+import {terser} from 'rollup-plugin-terser';
+
 export default {
-	input: 'src/main.js',
-	output: {
-	  file: 'bundle.js',
-	  format: 'cjs'
-	}
+	input: ['src/core.js'],
+	output: [
+		{
+			dir: 'js/build',
+			entryFileNames: 'lazysizes.[name].js',
+	  		format: 'iife'
+		},
+		{
+			dir: 'js/build',
+			entryFileNames: 'lazysizes.[name].min.js',
+			format: 'iife',
+			plugins: [terser()]
+		  }
+	],
+	plugins: [
+		resolve(),
+		legacy({ 'lazysizes/lazysizes.js': 'lazysizes' })
+	]
   };
