6d08abf23d15fe17669538d1891d8787d63e73d9
Patrick Sletvold <patricksletvold@hotmail.com>
2020-05-28 16:47:36 +0200

    Generate regular lazysizes files as well

--- rollup.prepare.config.js
+++ rollup.prepare.config.js
@@ -7,11 +7,18 @@ export default {
 		'lazysizes/plugins/native-loading/ls.native-loading.js',
 		'lazysizes/plugins/unveilhooks/ls.unveilhooks.js'
 	],
-	output: {
-		dir: 'js/src/lib',
-		entryFileNames: '[name].js',
-		format: 'es'
-	},
+	output: [
+		{
+			dir: 'js/src/lib',
+			entryFileNames: '[name].js',
+			format: 'es'
+		},
+		{
+			dir: 'js',
+			entryFileNames: '[name].js',
+			format: 'cjs'
+		}
+	],
 	plugins: [
 		resolve()
 	]
