65bd65f3b7d2b2a589f7867da9db159b72132de0
Patrick Sletvold <patricksletvold@hotmail.com>
2020-05-28 16:20:34 +0200

    Add unveil hooks and native loading

--- rollup.prepare.config.js
+++ rollup.prepare.config.js
@@ -3,7 +3,9 @@ import resolve from '@rollup/plugin-node-resolve';
 export default {
 	input: [
 		'lazysizes',
-		'lazysizes/plugins/aspectratio/ls.aspectratio.js'
+		'lazysizes/plugins/aspectratio/ls.aspectratio.js',
+		'lazysizes/plugins/native-loading/ls.native-loading.js',
+		'lazysizes/plugins/unveilhooks/ls.unveilhooks.js'
 	],
 	output: {
 		dir: 'js/src/lib',
