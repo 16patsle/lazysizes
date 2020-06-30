a6f5d361c79b84cca15efa1f6f4576d506577386
Patrick Sletvold <patricksletvold@hotmail.com>
2020-06-30 16:39:52 +0200

    Reusable blurhash process function and prevent reprocessing

--- js/src/blurhash.js
+++ js/src/blurhash.js
@@ -3,42 +3,49 @@ import decode from 'blurhash/dist/decode';
 function blurhashLoad() {
 	const blurhashImages = document.querySelectorAll('img[data-blurhash]');
 
-	blurhashImages.forEach(image => {
-		let width = image.width;
-		let height = image.height;
+	blurhashImages.forEach(processImage)
+}
 
-		if(width <= 1 || height <= 1) {
-			if(image.dataset.aspectratio) {
-				const aspectratio = image.dataset.aspectratio.split('/');
-				width = parseInt(aspectratio[0], 10);
-				height = parseInt(aspectratio[1], 10);
+function processImage(image) {
+	if(image.classList.contains('blurhashed')) {
+		return;
+	}
 
-				if(width <= 25 || height <= 25) {
-					// Probably an actual aspect ratio, we can't handel that yet.
-					return
-				}
-			} else {
-				return;
+	let width = image.width;
+	let height = image.height;
+
+	if(width <= 1 || height <= 1) {
+		if(image.dataset.aspectratio) {
+			const aspectratio = image.dataset.aspectratio.split('/');
+			width = parseInt(aspectratio[0], 10);
+			height = parseInt(aspectratio[1], 10);
+
+			if(width <= 25 || height <= 25) {
+				// Probably an actual aspect ratio, we can't handel that yet.
+				return
 			}
+		} else {
+			return;
 		}
+	}
 
-		const pixels = decode(image.dataset.blurhash, width, height);
-		console.log(image.dataset.blurhash, width, height);
-		image.classList.remove('lazyload');
+	const pixels = decode(image.dataset.blurhash, width, height);
+	console.log(image.dataset.blurhash, width, height);
+	image.classList.remove('lazyload');
 
-		const canvas = document.createElement('canvas');
-		canvas.width = width;
-		canvas.height = height;
-		const ctx = canvas.getContext('2d');
-		const imageData = ctx.createImageData(width, height);
-		imageData.data.set(pixels);
-		ctx.putImageData(imageData, 0, 0);
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	const ctx = canvas.getContext('2d');
+	const imageData = ctx.createImageData(width, height);
+	imageData.data.set(pixels);
+	ctx.putImageData(imageData, 0, 0);
 
-		canvas.toBlob(blob => {
-	  	const url = URL.createObjectURL(blob);
-		  image.src = url;
-		});
-	})
+	canvas.toBlob(blob => {
+	const url = URL.createObjectURL(blob);
+	  image.src = url;
+	  image.classList.add('blurhashed');
+	});
 }
 
 document.addEventListener('DOMContentLoaded', blurhashLoad);
