6fbc63687887e3671a49ff85774568e62cb64010
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-01 13:12:23 +0200

    Save blurhash in post meta

--- inc/Lazysizes/class-blurhash.php
+++ inc/Lazysizes/class-blurhash.php
@@ -23,57 +23,69 @@ class Blurhash {
 	 */
 	public static function get_blurhash( $url ) {
 		$attachment_id = attachment_url_to_postid( $url );
-		$metadata = wp_get_attachment_metadata( $attachment_id );
 
-		$size = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
-		if ( $size === false ) {
-			return false; // Probably not an image, might be video/audio.
-		}
-		$path = $size[0];
-		$width = $size[1];
-		$height = $size[2];
+		// Get from attachment post meta.
+		$blurhash = get_post_meta( $attachment_id, '_lazysizes_blurhash', true );
 
-		$pixels = array();
+		// Or generate if not already saved.
+		if ( $blurhash === '' ) {
+			$metadata = wp_get_attachment_metadata( $attachment_id );
 
-		if ( extension_loaded( 'imagick' ) ) {
-			$image = new \Imagick( $path );
-			$iterator = $image->getPixelIterator();
+			$size = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
+			if ( $size === false ) {
+				return false; // Probably not an image, might be video/audio.
+			}
+			$path = $size[0];
+			$width = $size[1];
+			$height = $size[2];
 
-			foreach ( $iterator as $imagePixels ) {
-				$row = array();
-				foreach ( $imagePixels as $pixel ) {
-					$colors = $pixel->getColor();
-					$row[] = [$colors['r'], $colors['g'], $colors['b']];
+			$pixels = array();
+
+			if ( extension_loaded( 'imagick' ) ) {
+				$image = new \Imagick( $path );
+				$iterator = $image->getPixelIterator();
+
+				foreach ( $iterator as $imagePixels ) {
+					$row = array();
+					foreach ( $imagePixels as $pixel ) {
+						$colors = $pixel->getColor();
+						$row[] = [$colors['r'], $colors['g'], $colors['b']];
+					}
+					$pixels[] = $row;
 				}
-				$pixels[] = $row;
+
+				$image->clear();
+			} else if ( extension_loaded( 'gd' ) ) {
+				$image = imagecreatefromstring( file_get_contents( $path ) );
+
+				for ($y = 0; $y < $height; ++$y) {
+					$row = array();
+					for ( $x = 0; $x < $width; ++$x ) {
+						$index = imagecolorat( $image, $x, $y );
+						$colors = imagecolorsforindex( $image, $index );
+
+						$row[] = [$colors['red'], $colors['green'], $colors['blue']];
+					}
+					$pixels[] = $row;
+				}
+
+				imagedestroy( $image );
+			} else {
+				return false; // Image manipulation not supported.
 			}
 
-			$image->clear();
-		} else if ( extension_loaded( 'gd' ) ) {
-			$image = imagecreatefromstring( file_get_contents( $path ) );
+			$components_x = 4;
+			$components_y = 3;
 
-			for ($y = 0; $y < $height; ++$y) {
-				$row = array();
-				for ( $x = 0; $x < $width; ++$x ) {
-					$index = imagecolorat( $image, $x, $y );
-					$colors = imagecolorsforindex( $image, $index );
+			set_time_limit( 60 );
 
-					$row[] = [$colors['red'], $colors['green'], $colors['blue']];
-				}
-				$pixels[] = $row;
-			}
+			// Generate Blurhash.
+			$blurhash = PhpBlurhash::encode( $pixels, $components_x, $components_y );
 
-			imagedestroy( $image );
-		} else {
-			return false; // Image manipulation not supported.
+			// Save in post meta for later.
+			add_post_meta( $attachment_id, '_lazysizes_blurhash', $blurhash, true );
 		}
 
-		$components_x = 4;
-		$components_y = 3;
-
-		set_time_limit( 60 );
-
-		// Blurhash
-		return PhpBlurhash::encode( $pixels, $components_x, $components_y );
+		return $blurhash;
 	}
 }
