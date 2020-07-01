307d862d6acb877edb296d6a0d1bca73c1b4b1fc
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-01 14:07:52 +0200

    Split blurhash function into fetching and generating

--- inc/Lazysizes/class-blurhash.php
+++ inc/Lazysizes/class-blurhash.php
@@ -15,10 +15,10 @@ defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
  */
 class Blurhash {
 	/**
-	 * Computes the Blurhash string
+	 * Fetches the Blurhash string from metadata, or computes it.
 	 *
 	 * @since 1.4.0
-	 * @param string      $url The attachment url, from src attribute.
+	 * @param string $url The attachment url, from src attribute.
 	 * @return string|false The Blurhash string, or false.
 	 */
 	public static function get_blurhash( $url ) {
@@ -30,61 +30,73 @@ class Blurhash {
 		// Or generate if not already saved.
 		if ( $blurhash === '' ) {
 			$metadata = wp_get_attachment_metadata( $attachment_id );
-
-			$size = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
-			if ( $size === false ) {
-				return false; // Probably not an image, might be video/audio.
-			}
-			$path = $size[0];
-			$width = $size[1];
-			$height = $size[2];
-
-			$pixels = array();
-
-			if ( extension_loaded( 'imagick' ) ) {
-				$image = new \Imagick( $path );
-				$iterator = $image->getPixelIterator();
-
-				foreach ( $iterator as $imagePixels ) {
-					$row = array();
-					foreach ( $imagePixels as $pixel ) {
-						$colors = $pixel->getColor();
-						$row[] = [$colors['r'], $colors['g'], $colors['b']];
-					}
-					$pixels[] = $row;
-				}
-
-				$image->clear();
-			} else if ( extension_loaded( 'gd' ) ) {
-				$image = imagecreatefromstring( file_get_contents( $path ) );
-
-				for ($y = 0; $y < $height; ++$y) {
-					$row = array();
-					for ( $x = 0; $x < $width; ++$x ) {
-						$index = imagecolorat( $image, $x, $y );
-						$colors = imagecolorsforindex( $image, $index );
-
-						$row[] = [$colors['red'], $colors['green'], $colors['blue']];
-					}
-					$pixels[] = $row;
-				}
-
-				imagedestroy( $image );
-			} else {
-				return false; // Image manipulation not supported.
-			}
-
-			$components_x = 4;
-			$components_y = 3;
-
-			set_time_limit( 60 );
-
-			// Generate Blurhash.
-			$blurhash = PhpBlurhash::encode( $pixels, $components_x, $components_y );
-
-			// Save in post meta for later.
-			add_post_meta( $attachment_id, '_lazysizes_blurhash', $blurhash, true );
+			$blurhash = self::encode_blurhash( $metadata, $attachment_id );
 		}
+
+		return $blurhash;
+	}
+
+	/**
+	 * Computes the Blurhash string.
+	 * @since 1.4.0
+	 * @param array $metadata An array of attachment metadata.
+	 * @param int   $attachment_id Current attachment ID.
+	 * @return string|false The Blurhash string, or false.
+	 */
+	public static function encode_blurhash( $metadata, $attachment_id ) {
+		$size = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
+		if ( $size === false ) {
+			return false; // Probably not an image, might be video/audio.
+		}
+		$path = $size[0];
+		$width = $size[1];
+		$height = $size[2];
+
+		$pixels = array();
+
+		if ( extension_loaded( 'imagick' ) ) {
+			$image = new \Imagick( $path );
+			$iterator = $image->getPixelIterator();
+
+			foreach ( $iterator as $imagePixels ) {
+				$row = array();
+				foreach ( $imagePixels as $pixel ) {
+					$colors = $pixel->getColor();
+					$row[] = [$colors['r'], $colors['g'], $colors['b']];
+				}
+				$pixels[] = $row;
+			}
+
+			$image->clear();
+		} else if ( extension_loaded( 'gd' ) ) {
+			$image = imagecreatefromstring( file_get_contents( $path ) );
+
+			for ($y = 0; $y < $height; ++$y) {
+				$row = array();
+				for ( $x = 0; $x < $width; ++$x ) {
+					$index = imagecolorat( $image, $x, $y );
+					$colors = imagecolorsforindex( $image, $index );
+
+					$row[] = [$colors['red'], $colors['green'], $colors['blue']];
+				}
+				$pixels[] = $row;
+			}
+
+			imagedestroy( $image );
+		} else {
+			return false; // Image manipulation not supported.
+		}
+
+		$components_x = 4;
+		$components_y = 3;
+
+		set_time_limit( 60 );
+
+		// Generate Blurhash.
+		$blurhash = PhpBlurhash::encode( $pixels, $components_x, $components_y );
+
+		// Save in post meta for later.
+		add_post_meta( $attachment_id, '_lazysizes_blurhash', $blurhash, true );
 
 		return $blurhash;
 	}
