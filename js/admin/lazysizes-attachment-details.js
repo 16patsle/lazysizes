b0806cbdbb2c5cee9e48314294a45365b0a26de9
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-04 13:52:57 +0200

    Add attachment extension to plain details view

--- js/admin/lazysizes-attachment-details.js
+++ js/admin/lazysizes-attachment-details.js
@@ -1,11 +1,32 @@
 var templateString = "<span class='setting custom-thing'>" +
 	                        "<span class='name'>Blurhash</span>" +
 	                        "<span class='value'><%= lazysizesBlurhash %></span>" +
-	                        '</span>';
+                            '</span>';
+
+var templateFunction = _.template(templateString);
 
 // Based on code by Thomas Griffin.
 // See https://gist.github.com/sunnyratilal/5650341.
-var extendObject = {
+wp.media.view.Attachment.Details.TwoColumn = wp.media.view.Attachment.Details.TwoColumn.extend({
+    initialize: function(){
+        wp.media.view.Attachment.Details.prototype.initialize.apply(this, arguments);
+        // Always make sure that our content is up to date.
+        this.listenTo(this.model, 'change', this.render);
+    },
+    render: function(){
+        // Ensure that the main attachment fields are rendered.
+        wp.media.view.Attachment.prototype.render.apply(this, arguments);
+
+        // Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
+        this.views.detach();
+		this.$el.find( '.settings' ).append(templateFunction(this.model.toJSON()));
+        this.model.fetch();
+        this.views.render();
+
+        return this;
+    }
+});
+wp.media.view.Attachment.Details = wp.media.view.Attachment.Details.extend({
     initialize: function(){
         // Always make sure that our content is up to date.
         this.listenTo(this.model, 'change', this.render);
@@ -16,14 +37,10 @@ var extendObject = {
 
         // Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
         this.views.detach();
-		this.$el.find( '.settings' ).append(_.template(templateString)(this.model.toJSON()));
-		console.log(this.model.toJSON());
+		this.$el.append(templateFunction(this.model.toJSON()));
         this.model.fetch();
         this.views.render();
 
         return this;
     }
-}
-
-wp.media.view.Attachment.Details.TwoColumn = wp.media.view.Attachment.Details.TwoColumn.extend(extendObject);
-wp.media.view.Attachment.Details = wp.media.view.Attachment.Details.extend(extendObject);
+});
