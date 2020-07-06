0f1b69d5d55f2fb3d3b06eed8ee28f400f5861e7
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-06 14:40:05 +0200

    Use es6 classes, like in blurhash decode source

--- js/src/lib/decode.js
+++ js/src/lib/decode.js
@@ -116,29 +116,13 @@ utils.signPow = function (val, exp) {
     return (val < 0 ? -1 : 1) * Math.pow(Math.abs(val), exp);
 };
 
-var __extends = (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var ValidationError = /** @class */ (function (_super) {
-    __extends(ValidationError, _super);
-    function ValidationError(message) {
-        var _this = _super.call(this, message) || this;
-        _this.name = "ValidationError";
-        _this.message = message;
-        return _this;
-    }
-    return ValidationError;
-}(Error));
+class ValidationError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = 'ValidationError';
+		this.message = message;
+	}
+}
 
 /**
  * Returns an error message if invalid or undefined if valid
