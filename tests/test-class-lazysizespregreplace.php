b3e6cc34e8be950e08122cfe0aff2e941ba132c8
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-09 20:47:41 +0200

    Fix tests for aspect ratio

--- tests/test-class-lazysizespregreplace.php
+++ tests/test-class-lazysizespregreplace.php
@@ -494,7 +494,7 @@ class Tests_LazysizesPregReplace extends WP_UnitTestCase {
 	 * Test if the right aspect ratio is added.
 	 */
 	public function test_should_add_aspect_ratio() {
-		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" width="100px" height="200px">' );
+		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" width="100px" height="200px">', 'image.jpg', 'img' );
 		$expected = '<img src="image.jpg" data-aspectratio="100/200" width="100px" height="200px">';
 
 		$this->assertEquals( $expected, $markup );
@@ -504,7 +504,7 @@ class Tests_LazysizesPregReplace extends WP_UnitTestCase {
 	 * Test if no aspect ratio is added because of missing width.
 	 */
 	public function test_should_not_add_aspect_ratio_width_missing() {
-		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" height="200px">' );
+		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" height="200px">', 'image.jpg', 'img' );
 		$expected = '<img src="image.jpg" height="200px">';
 
 		$this->assertEquals( $expected, $markup );
@@ -514,7 +514,7 @@ class Tests_LazysizesPregReplace extends WP_UnitTestCase {
 	 * Test if no aspect ratio is added because of missing height.
 	 */
 	public function test_should_not_add_aspect_ratio_height_missing() {
-		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" width="100px">' );
+		$markup   = $this->class_instance->set_aspect_ratio( '<img src="image.jpg" width="100px">', 'image.jpg', 'img' );
 		$expected = '<img src="image.jpg" width="100px">';
 
 		$this->assertEquals( $expected, $markup );
