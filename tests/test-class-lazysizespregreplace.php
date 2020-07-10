ad2c7efb20a808ff9f7712d1cc38254626074340
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-10 18:03:33 +0200

    Add WIP tests for blurhash

--- tests/test-class-lazysizespregreplace.php
+++ tests/test-class-lazysizespregreplace.php
@@ -537,5 +537,53 @@ class Tests_LazysizesPregReplace extends WP_UnitTestCase {
 		$this->assertEquals( $expected, $markup );
 	}
 
+	/**
+	 * Test that a Blurhash string is added in the data-blurhash attribute when the src is an attachment.
+	 */
+	public function test_should_add_blurhash_attribute() {
+		// Create custom class instance with blurhash on load enabled.
+		$class_instance = new PregReplace(
+			array(
+				'excludeclasses'  => array(),
+				'skip_src'        => false,
+				'blurhash'        => true,
+				'blurhash_onload' => true,
+			),
+			dirname( __FILE__ )
+		);
 
+		$attachment_id = self::factory()->attachment->create_upload_object( __DIR__ . '/test-pineapple.jpg' );
+
+		$url = wp_get_attachment_url( $attachment_id );
+
+		$markup   = $class_instance->preg_replace_html( '<img src="' . $url . '" srcset="something" alt="Image" width="300px" height="400px">', array( 'img' ) );
+		$expected = '<img data-blurhash="???" data-aspectratio="300/400" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="' . $url . '" data-srcset="something" alt="Image" width="300px" height="400px" class="lazyload"><noscript><img src="' . $url . '" srcset="something" alt="Image" width="300px" height="400px"></noscript>';
+
+		if ( version_compare( phpversion(), '7.2', '<' ) ) {
+			$expected = '<img data-aspectratio="300/400" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="' . $url . '" data-srcset="something" alt="Image" width="300px" height="400px" class="lazyload"><noscript><img src="' . $url . '" srcset="something" alt="Image" width="300px" height="400px"></noscript>';
+		}
+
+		$this->assertEquals( $expected, $markup );
+	}
+
+	/**
+	 * Test that no data-blurhash attribute is added when the src is not an attachment.
+	 */
+	public function test_should_not_add_blurhash_attribute_no_attachment() {
+		// Create custom class instance with blurhash on load enabled.
+		$class_instance = new PregReplace(
+			array(
+				'excludeclasses'  => array(),
+				'skip_src'        => false,
+				'blurhash'        => true,
+				'blurhash_onload' => true,
+			),
+			dirname( __FILE__ )
+		);
+
+		$markup   = $class_instance->preg_replace_html( '<img src="image.jpg" srcset="something" alt="Image" width="300px" height="400px">', array( 'img' ) );
+		$expected = '<img data-aspectratio="300/400" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="image.jpg" data-srcset="something" alt="Image" width="300px" height="400px" class="lazyload"><noscript><img src="image.jpg" srcset="something" alt="Image" width="300px" height="400px"></noscript>';
+
+		$this->assertEquals( $expected, $markup );
+	}
 }
