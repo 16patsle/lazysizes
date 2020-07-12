1b1b5eeb12eb0fac34d466e416d1ba4a46e05339
Patrick Sletvold <patricksletvold@hotmail.com>
2020-07-12 19:35:01 +0200

    Fix phpcs errors

--- scoper.inc.php
+++ scoper.inc.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * Configuration file for PHP-Scoper
+ *
+ * @package Lazysizes
+ */
 
+// phpcs:disable PHPCompatibility.ControlStructures.NewExecutionDirectives.strict_typesFound
 declare(strict_types=1);
 
 use Isolated\Symfony\Component\Finder\Finder;
@@ -12,12 +18,11 @@ return array(
 	// directory. You can however define which files should be scoped by defining a collection of Finders in the
 	// following configuration key.
 	//
-	// For more see: https://github.com/humbug/php-scoper#finders-and-paths
+	// For more see: https://github.com/humbug/php-scoper#finders-and-paths.
 	'finders' => array(
 		Finder::create()
 			->files()
 			->ignoreVCS( true )
-			// ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
 			->notName( '/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock|\\.xml|\\.yml|\\.ini/' )
 			->exclude(
 				array(
