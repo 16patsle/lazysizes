a7c29b3eaf0affc8373327d7979e8133cb1c4978
Patrick Sletvold <patricksletvold@hotmail.com>
2020-08-10 20:34:17 +0200

    Format attachment details code

--- js/admin/src/lazysizes-attachment-details.js
+++ js/admin/src/lazysizes-attachment-details.js
@@ -30,11 +30,11 @@ const templateString = `
 
 const templateFunction = _.template(templateString);
 
-const clickFunction = function(e) {
+const clickFunction = function (e) {
 	let action = '';
-	if(e.target.classList.contains('lazysizes-blurhash-generate')) {
+	if (e.target.classList.contains('lazysizes-blurhash-generate')) {
 		action = 'generate';
-	} else if(e.target.classList.contains('lazysizes-blurhash-delete')) {
+	} else if (e.target.classList.contains('lazysizes-blurhash-delete')) {
 		action = 'delete';
 	} else {
 		return;
@@ -42,90 +42,104 @@ const clickFunction = function(e) {
 
 	this.model.set('lazysizesLoading', true);
 
-	lazysizesAjax(action, this.model.attributes.id, this.model.attributes.nonces.lazysizes[action], (response, status, errorCode) => {
-		this.model.set('lazysizesLoading', false);
+	lazysizesAjax(
+		action,
+		this.model.attributes.id,
+		this.model.attributes.nonces.lazysizes[action],
+		(response, status, errorCode) => {
+			this.model.set('lazysizesLoading', false);
 
-		if(status === 'error') {
-			this.model.set('lazysizesError', `${lazysizesStrings.error} (${errorCode})`)
-		} else {
-			if (response.success) {
-				if (action === 'generate') {
-					this.model.set('lazysizesBlurhash', response.blurhash)
-				} else if (action === 'delete') {
-					this.model.set('lazysizesBlurhash', false)
-				}
+			if (status === 'error') {
+				this.model.set(
+					'lazysizesError',
+					`${lazysizesStrings.error} (${errorCode})`
+				);
 			} else {
-				this.model.set('lazysizesError', response.data[0].message)
+				if (response.success) {
+					if (action === 'generate') {
+						this.model.set('lazysizesBlurhash', response.blurhash);
+					} else if (action === 'delete') {
+						this.model.set('lazysizesBlurhash', false);
+					}
+				} else {
+					this.model.set('lazysizesError', response.data[0].message);
+				}
 			}
 		}
-	})
-}
+	);
+};
 
 // Based on code by Thomas Griffin.
 // See https://gist.github.com/sunnyratilal/5650341.
 
-const mediaTwoColumn = wp.media.view.Attachment.Details.TwoColumn
+const mediaTwoColumn = wp.media.view.Attachment.Details.TwoColumn;
 
 // In Media Library.
 wp.media.view.Attachment.Details.TwoColumn = mediaTwoColumn.extend({
-    initialize: function(){
+	initialize: function () {
 		mediaTwoColumn.prototype.initialize.apply(this, arguments);
 
-        // Always make sure that our content is up to date.
+		// Always make sure that our content is up to date.
 		this.listenTo(this.model, 'change', this.render);
 	},
 	events: {
-		'click .setting.lazysizes-blurhash .button': clickFunction
+		'click .setting.lazysizes-blurhash .button': clickFunction,
 	},
-    render: function(){
-        // Ensure that the main attachment fields (and the fields of other plugins) are rendered.
+	render: function () {
+		// Ensure that the main attachment fields (and the fields of other plugins) are rendered.
 		mediaTwoColumn.prototype.render.apply(this, arguments);
 
-		if(this.model.changedAttributes(['lazysizesBlurhash', 'lazysizesError']) === false) {
+		if (
+			this.model.changedAttributes(['lazysizesBlurhash', 'lazysizesError']) ===
+			false
+		) {
 			this.model.fetch();
 		}
 
-        // Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
-        this.views.detach();
-		if(this.model.attributes.type === 'image') {
-			this.$el.find( '.settings' ).append(templateFunction(this.model.toJSON()));
+		// Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
+		this.views.detach();
+		if (this.model.attributes.type === 'image') {
+			this.$el.find('.settings').append(templateFunction(this.model.toJSON()));
 		}
-        this.views.render();
+		this.views.render();
 
-        return this;
-    }
+		return this;
+	},
 });
 
-const mediaAttachmentDetails = wp.media.view.Attachment.Details
+const mediaAttachmentDetails = wp.media.view.Attachment.Details;
 
 // In post editor, when selecting attachment.
 wp.media.view.Attachment.Details = mediaAttachmentDetails.extend({
-    initialize: function(){
+	initialize: function () {
 		mediaAttachmentDetails.prototype.initialize.apply(this, arguments);
 
-        // Always make sure that our content is up to date.
+		// Always make sure that our content is up to date.
 		this.listenTo(this.model, 'change', this.render);
 	},
 	events: {
-		'click .setting.lazysizes-blurhash .button': clickFunction
+		'click .setting.lazysizes-blurhash .button': clickFunction,
 	},
-    render: function(){
-        // Ensure that the main attachment fields (and the fields of other plugins) are rendered.
+	render: function () {
+		// Ensure that the main attachment fields (and the fields of other plugins) are rendered.
 		mediaAttachmentDetails.prototype.render.apply(this, arguments);
 
-		if(this.model.changedAttributes(['lazysizesBlurhash', 'lazysizesError']) === false) {
+		if (
+			this.model.changedAttributes(['lazysizesBlurhash', 'lazysizesError']) ===
+			false
+		) {
 			this.model.fetch();
 		}
 
-        // Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
+		// Detach the views, append our custom fields, make sure that our data is fully updated and re-render the updated view.
 		this.views.detach();
-		if(this.model.attributes.type === 'image') {
+		if (this.model.attributes.type === 'image') {
 			this.$el.append(templateFunction(this.model.toJSON()));
 		}
-        this.views.render();
+		this.views.render();
 
-        return this;
-    }
+		return this;
+	},
 });
 
 function lazysizesAjax(action, attachmentId, nonce, callback) {
@@ -136,9 +150,9 @@ function lazysizesAjax(action, attachmentId, nonce, callback) {
 			action: 'lazysizes_blurhash',
 			nonce,
 			mode: action,
-			attachmentId
+			attachmentId,
 		},
 		success: callback,
-		error: callback
-	})
+		error: callback,
+	});
 }
